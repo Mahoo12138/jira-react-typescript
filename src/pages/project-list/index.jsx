11ed0bccfa8dcf6c432d126c9d5434e731f63c45
Mahoo12138 <mahoo12138@outlook.com>
2022-01-17 10:41:17 +0800

    feat: 添加 useMount 和 useDebounce

--- src/pages/project-list/index.jsx
+++ src/pages/project-list/index.jsx
@@ -1,36 +1,67 @@
-import { useState,useEffect } from "react";
-import qs from 'qs'
-import SearchPanel from './search-panel'
-import List from './list'
-import { cleanObject } from "../../utils";
+import { useState, useEffect } from "react";
+import qs from "qs";
+import SearchPanel from "./search-panel";
+import List from "./list";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 
-const apiUrl = process.env.REACT_APP_API_URL
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListPage = () => {
+  console.log("component start");
   const [param, setParam] = useState({
     name: "",
-    personId: ""
-  })
-  const [list, setList] = useState([])
-  const [users, setUsers] = useState([])
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(`${apiUrl}/users`).then(async response => {
+  //     if(response.ok){
+  //       setUsers(await response.json())
+  //     }
+  //   })
+  // }, [])  // 只触发一次
+  useMount(() => {
+    console.log("users data start");
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        console.log("setUsers start");
+        setUsers(await response.json());
+        console.log("setUsers over");
+      }
+    });
+    console.log("users data over");
+  });
+
+  const debouncedParam = useDebounce(param, 3000);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
-      if(response.ok){
-        setList(await response.json())
-      }
-    })
-  }, [param])
+    console.log("project data start");
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        console.log("setList start");
+        setList(await response.json());
+        console.log("setList over");
 
-  useEffect(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
-      if(response.ok){
-        setUsers(await response.json())
       }
-    })
-  }, [])  // 只触发一次
-  return (<div>
-    <SearchPanel param={param} setParam={setParam} users={users} setUsers={setUsers}/>
-    <List list={list} users={users}/>
-  </div>)
-}
+    });
+    console.log("project data over");
+  }, [debouncedParam]);
+
+  console.log("component over");
+  return (
+    <div>
+      <SearchPanel
+        param={param}
+        setParam={setParam}
+        users={users}
+        setUsers={setUsers}
+      />
+      {console.log("dom ")}
+      <List list={list} users={users} />
+    </div>
+  );
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 export const cleanObject = (obj) => {
@@ -9,4 +11,33 @@ export const cleanObject = (obj) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  // console.log("useMount start")
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  // console.log("useMount over")
+};
+
+export const useDebounce = (value, delay) => {
+  // console.log("useDebounce start", value)
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // console.log("debounce Effect start")
+    const timeout = setTimeout(() => {
+      // console.log("setDebouncedValue")
+      setDebouncedValue(value);
+    }, delay);
+    // console.log("debounce Effect over")
+    return () => {
+      // console.log("clearTimeout")
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+  // console.log("useDebounce over")
+  return debouncedValue;
 };
